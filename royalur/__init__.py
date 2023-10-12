fa0b6484d3a7c2758e0a8f733e7d3261a85fcb19
Padraig Lamont <padraiglamont@gmail.com>
2023-10-12 11:30:05 +0100

    Re-export common classes from royalur package

--- royalur/__init__.py
+++ royalur/__init__.py
@@ -1,3 +1,9 @@
 """
 This package contains the source code for RoyalUr.
 """
+
+from .model import PlayerType, Tile
+from .path import PathPair, PathType
+from .shape import BoardShape, BoardType
+from .board import Piece, Move
+from .dice import Dice, DiceType
