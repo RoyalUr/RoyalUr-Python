51c8f5a0290b33231c9fb406f80588ea1eaab0db
Padraig Lamont <padraiglamont@gmail.com>
2023-10-12 12:58:21 +0100

    Refactor model.py into player.py and tile.py

--- royalur/model/__init__.py
+++ royalur/model/__init__.py
@@ -2,3 +2,10 @@
 This package contains the model for storing the state
 of games of the Royal Game of Ur.
 """
+
+from .tile import Tile
+from .player import PlayerType
+from .path import PathPair, PathType
+from .shape import BoardShape, BoardType
+from .board import Piece, Move
+from .dice import Dice, DiceType
