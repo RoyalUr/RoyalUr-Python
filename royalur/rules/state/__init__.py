d0781e417f8aa44940cb1e9a074e42331e6a2fe0
Padraig Lamont <padraiglamont@gmail.com>
2023-10-15 13:02:19 +0100

    Re-export state classes

--- royalur/rules/state/__init__.py
+++ royalur/rules/state/__init__.py
@@ -1,3 +1,19 @@
 """
 This package contains representations of the states of games.
 """
+
+from .state import (
+    GameState,
+    OngoingGameState,
+    WinGameState,
+)
+from .action import (
+    ActionGameState,
+    RolledGameState,
+    MovedGameState,
+)
+from .playable import (
+    PlayableGameState,
+    WaitingForRollGameState,
+    WaitingForMoveGameState,
+)
